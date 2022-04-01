9e1207c937ff0a962c97a1a561fe410ad307219f
shopnobaz <vision2025bd@yahoo.com>
2022-04-01 14:54:46 +0200

    describe

--- test/mock.test.js
+++ test/mock.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('@jest/globals');
-desscribe('A test suite', () => {
+describe('A test suite', () => {
 
     test("A fake test", () => {
         expect(3).toBe(3); //failing on puropse
